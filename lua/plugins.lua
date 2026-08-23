8de2699f5f6230ef380fb8d6db621c01e8442b60
fedorkobak <kobfedsur@gmail.com>
2026-08-23 23:45:33 +0300

    add plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -106,4 +106,13 @@ return {
 
         end,
     },
+    {
+        "git@github.com:fedorkobak/md_runner.nvim.git",
+        config = function()
+            local md_runner = require("md_runner")
+            md_runner.setup({
+                log_level = vim.log.levels.WARNING,
+            })
+        end
+    },
 }
