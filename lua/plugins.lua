ae22466791521457aa16587cd2e0005a8d28bb44
fedorkobak <kobfedsur@gmail.com>
2026-08-30 00:15:03 +0300

    remove md_runner

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -114,13 +114,13 @@ return {
 
         end,
     },
-    {
-        "git@github.com:fedorkobak/md_runner.nvim.git",
-        config = function()
-            local md_runner = require("md_runner")
-            md_runner.setup({
-                log_level = vim.log.levels.WARNING,
-            })
-        end
-    },
+    -- {
+    --     "git@github.com:fedorkobak/md_runner.nvim.git",
+    --     config = function()
+    --         local md_runner = require("md_runner")
+    --         md_runner.setup({
+    --             log_level = vim.log.levels.WARNING,
+    --         })
+    --     end
+    -- },
 }
