bd4c8ab79fa9623b5a50ec2a9f45421f2a38289d
fedorkobak <kobfedsur@gmail.com>
2026-08-30 16:07:56 +0300

    updated telescope shortcuts

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -26,7 +26,7 @@ return {
             local live_grep = function()
                 builtin.live_grep({ hidden = true, no_ignore = true })
             end
-            vim.keymap.set("n", "<C-P>", live_grep, {})
+            vim.keymap.set("n", "<S-p>", live_grep, {})
         end,
     },
     {
